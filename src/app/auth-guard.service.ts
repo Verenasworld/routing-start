18bc20438863d7776a438e27286f765dd4aa0175
Verenasworld <verena.felizidas@gmail.com>
2020-03-31 21:25:24 +0200

    add Auth Guard

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,15 +1,29 @@
-import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
-import {Observable} from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router} from '@angular/router';
+
 import {AuthService} from './auth.service';
-import {state} from '@angular/animations';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService, private route: Router ) { }
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  // @ts-ignore
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot) {
+    this.authService.isAuthenticated()
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+            } else {
+            this.router.navigate(['/']);
+          }
+        }
+      );
+  }
 }
-canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree;
-}
-}
+
